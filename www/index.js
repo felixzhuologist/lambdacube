12edc77d3dabf4fba1f95558dde25c73225d838e
felixzhuologist <felix.czhu@gmail.com>
2018-09-17 09:28:07 -0400

    add input/output elements to the page

--- www/index.js
+++ www/index.js
@@ -1,14 +1,35 @@
 import "@babel/polyfill";
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const Repl = (props) => (
+    <div>
+        <textarea id="repl-input">
+        </textarea>
+        <input
+            type="submit"
+            value="run"
+            onClick={() => {
+                const result = document.getElementById("result");
+                const input = document.getElementById("repl-input").value;
+                result.innerHTML = props.eval_code(input);
+            }}
+        />
+        <p id="result" />
+    </div>
+)
+
+Repl.propTypes = {
+    eval_code: PropTypes.func.isRequired
+};
 
 const run = async () => {
 const lib = import('./lambdacube');
 
 const { eval_code } = await lib;
-console.log(eval_code("22 * 10 + 3"));
 ReactDOM.render(
-    <p>hello</p>,
+    <Repl eval_code={eval_code} />,
     document.getElementById('root')
 )
 
