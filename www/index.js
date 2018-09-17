252af4487221f22bde3f62ae2839052b824d5607
felixzhuologist <felix.czhu@gmail.com>
2018-09-16 21:15:32 -0400

    add react to project

--- www/index.js
+++ www/index.js
@@ -1,9 +1,16 @@
-const lib = import('./lambdacube');
+import "@babel/polyfill";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const run = async () => {
+const lib = import('./lambdacube');
 
 const { eval_code } = await lib;
 console.log(eval_code("22 * 10 + 3"));
+ReactDOM.render(
+    <p>hello</p>,
+    document.getElementById('root')
+)
 
 }
 
