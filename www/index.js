77d59c3bc26570b5c3f4ac85faeda2b1e89a5664
felixzhuologist <felix.czhu@gmail.com>
2018-10-11 16:43:29 -0400

    remove react

--- www/index.js
+++ www/index.js
@@ -1,37 +1,15 @@
-import "@babel/polyfill";
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-
-const Repl = (props) => (
-    <div>
-        <textarea id="repl-input">
-        </textarea>
-        <input
-            type="submit"
-            value="run"
-            onClick={() => {
-                const result = document.getElementById("result");
-                const input = document.getElementById("repl-input").value;
-                result.innerHTML = props.eval_code(input);
-            }}
-        />
-        <p id="result" />
-    </div>
-)
-
-Repl.propTypes = {
-    eval_code: PropTypes.func.isRequired
-};
-
-const run = async () => {
 const lib = import('./lambdacube');
 
+const run = async () => {
+
 const { eval_simple } = await lib;
-ReactDOM.render(
-    <Repl eval_code={eval_simple} />,
-    document.getElementById('root')
-)
+
+const submitButton = document.getElementById('repl-submit')
+submitButton.addEventListener('click', event => {
+    const result = document.getElementById("result");
+    const input = document.getElementById("repl-input").value;
+    result.innerHTML = eval_simple(input);
+})
 
 }
 
--- www/webpack.config.js
+++ www/webpack.config.js
@@ -2,18 +2,6 @@ const path = require('path');
 
 module.exports = {
     entry: './index.js',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx', '.wasm']
-    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
