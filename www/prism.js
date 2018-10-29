0e1b84341b54fd573999640bbec859189da230da
felixzhuologist <felix.czhu@gmail.com>
2018-10-29 16:15:13 -0400

    get syntax highlighting working

--- www/prism.js
+++ www/prism.js
@@ -1,13 +1,12 @@
-// TODO: get this actually working
+// for some reason aliases don't seem to work
 Prism.languages.typezoo = {
-    'number': /\b[0-9]+/,
-    'type': {
-        pattern: /\b[A-Z]\w*\b/,
-        alias: 'function'
-    },
-    'variable': /\b[a-z_]\w*/,
-    'keyword': /\b(?:let|open|as|type|if|then|else|in|lin|ops|module|lambda|fun|λ|tyfun|sig|val|end|∀|forall|poly)\b/,
+    'number': /\b[0-9]+\b/,
+    'keyword': /\b(?:let|open|as|type|if|then|else|in|lin|ops|module|lambda|fun|λ|sig|val|end|∀|forall|poly)\b/,
     'boolean': /\b(?:false|true)\b/,
-    'operator': /\b(?:and|or|not|=|->|=>|<:)\b/,
+    // operator
+    'symbol': /\band|or|not|=|->|=>|<:|\+|-|<|<=|>|>=|%|\/|\*\b/,
+    // type
+    'string': /\b[A-Z]\w*|\btyfun/,
+    'variable': /[a-z_]\w*/,
     'punctuation': /[(){}\[\].,:;]/
 };
